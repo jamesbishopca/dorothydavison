5c6f7cbcf6ab72c3b7a638e27ba5b4a3aaa494e1
James Bishop <james@jamesbishop.ca>
2016-12-27 19:08:23 -0400

    update theming

--- _hugo/themes/hugo-creative-theme/static/js/creative.js
+++ _hugo/themes/hugo-creative-theme/static/js/creative.js
@@ -20,7 +20,7 @@
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -31,7 +31,7 @@
     $("h1").fitText(
         1.2, {
             minFontSize: '35px',
-            maxFontSize: '65px'
+            maxFontSize: '48px'
         }
     );
 
@@ -40,7 +40,7 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
